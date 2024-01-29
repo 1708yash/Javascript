199d86230425159f50da9ae4d6e70a15036e898b
Yash <140030060+teamsProjectsem5@users.noreply.github.com>
2024-01-30 03:44:53 +0530

    starting again

--- index.js
+++ index.js
@@ -1,114 +1,44 @@
-// console.log("hello this is yash");
-// window.alert("you are not allowed");
-// //comments are here
+// context and prototyping
+const vehicle = function (make, model) {
 
-// ways to create variables
-// declaration (var,let,const)
-// assignment (=assignment operator)
-// let age=19;
-// let student=true;
-// let firstname="yash";
-// console.log("Hello",firstname,"welcome here");
-// console.log("your age is:",age);
-// console.log("enrolled:",student);
+  this.make = make;
+  this.model = model;
+  this.print = function () {
+    return `${this.make} ${this.model}`;
+  };
+};
+const charger = new vehicle("dodge", "charger");
+const prius = new vehicle("toyota", "prius");
+console.log(prius.print());
+prius.__proto__ === charger.__proto__ // true
+vehicle.prototype.getmaxspeed = function () {
+  return 100;
+};
+console.log(charger.getmaxspeed());
 
-// document.getElementById("p1").innerHTML="hello "+firstname;
+console.log('asynchrony and callbacks')
+// used for adjusting server communication and latency in the fetch and request
+// example of callback
 
+const config={
 
+    acceptreq:true
+}
+const promise=new Promise((resolve,reject)=>{
+    if(config.acceptreq){
+        resolve('accepted')
+        console.log('accepted')
+    }else{
+        reject('rejected')
+        console.log('rejected')
+    }
+})
+const lala = function(){
+    console.log('lala')
+}
+//example of async function
+const userprofile= async function(){
+    let user= await promise
+    let userprofiles= await fetch('https://jsonplaceholder.typicode.com/users')
 
-
-
-//window promept in js
-// let username=window.prompt("who are you here?");
-// console.log(username);
-
-
-//window prompt using html 
-
-// document.getElementById("mybutton").onclick=function(){
-//     username=document.getElementById("mytext").value;
-//     console.log(username);
-//     document.getElementById("mylabel").innerHTML=username; 
-// }
-
-
-// const variable5
-//circumference
-// const pi=3.14;
-// let raduis;
-// let circum;
-
-// raduis=window.prompt("Enter the radius of circle:");
-// raduis=Number(raduis);
-// circum=2*pi*raduis;
-// console.log("the circumference is:", circum);
-// document.getElementById("p1").innerHTML=circum;
-
-
-
-// taking inpput and using maths function in js eg pythagoras theorm
-// let a,b,c;
-// a=window.prompt("Enter the side of a:");
-// a=Number(a);
-
-// b = window.prompt("Enter the side of b:");
-// b = Number(b);
-
-// c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
-// console.log("side c will be:",c);
-
-//pytha using html embade
-// document.getElementById("submitbutton").onclick=function(){
-
-// a=document.getElementById("atextb").value;
-// a=Number(a);
-
-// b = document.getElementById("btextb").value;
-// b = Number(b);
-
-// c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
-// document.getElementById("c").innerHTML="side c is: "+c;
-// console.log(c);
-
-// }
-// let count=0;
-// document.getElementById("decrease").onclick=function(){
-// count-=1;
-// document.getElementById("counter").innerHTML=count;
-// }
-
-// document.getElementById("increase").onclick = function () {
-// count+=1;
-//     document.getElementById("counter").innerHTML = count;
-// }
-
-// document.getElementById("reset").onclick = function () {
-// count=0;
-//     document.getElementById("counter").innerHTML = count;
-// }
-
-
-
-// let x=Math.random()*6;// this is used for getting any random number from the system itself and the programmer need not to thing or process it.
-// console.log(x)C
-
-
-//function in javascript
-// this is the simple function to be made using a name to the function
-// function fun(){
-//     var a=10;
-//     var b=20;
-//     var sum=a+b;
-// } 
-
-
-// anonymous function which do nt hava a name but is associated with a variable to handle the dunction
-var func = function fun(a,b) {
-  return total=a+b;
-} 
-//to call this function we do as :
-console.log(func(13,34));
-
-
-// starting the new coouse by harry here
-// studied about the variables and the data types in js and also
+}
